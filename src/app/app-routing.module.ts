0c4205327daa928aea782355a164b4d9977a7447
Andrés Torres Albuja <bryan.torres01@epn.edu.ec>
2023-03-17 12:27:10 -0500

    Estructura del proyecto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./screens/screens.module').then((m) => m.ScreensModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
